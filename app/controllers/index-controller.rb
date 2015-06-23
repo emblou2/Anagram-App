6056b798c6186f223acba6a4cf95ebd724adf98f
Emily Blanchard <emblou2@aol.com>
2015-06-23 14:40:55 -0600

    handle constraints

--- app/controllers/index-controller.rb
+++ app/controllers/index-controller.rb
@@ -1,8 +1,22 @@
+require 'sinatra'
+
 get '/' do
   erb :index
 end
 
+def valid_input(input)
+  if input.length > 3
+     raise Exception.new("Word must be less than or equal to 3 characters.")
+  end
+end
 
+def distinct_letters(input)
+   letter_array = input.chars
+   unique_letters = letter_array.uniq
+   if unique_letters.length < letter_array.length
+    raise Exception.new("Word must contain three distinct letters.")
+  end
+end
 
 get '/anagrams/:word' do
   @word = params[:word]
@@ -11,6 +25,13 @@ get '/anagrams/:word' do
 end
 
 post '/' do
-  @word = params[:word]
-  redirect "/anagrams/#{@word}"
+    word = params[:word]
+    begin 
+      valid_input(word)
+      distinct_letters(word)
+      redirect "/anagrams/#{word}"
+    rescue Exception => error
+      @error = error.message
+      erb :index
+    end
 end
