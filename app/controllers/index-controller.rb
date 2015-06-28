524aab3355fa53fdd5c53e37e3d9f424f1967e5b
Emily Blanchard <emblou2@aol.com>
2015-06-28 16:23:42 -0600

    added anagram.text

--- app/controllers/index-controller.rb
+++ app/controllers/index-controller.rb
@@ -11,7 +11,7 @@ def valid_input(input)
     raise Exception.new("Word must contain three distinct letters.")
 
   elsif input.length > 3
-     raise Exception.new("Word must be less than or equal to 3 characters.")
+     raise Exception.new("Word must be equal to 3 characters.")
   end
 end
 
@@ -19,7 +19,7 @@ end
 
 get '/anagrams/:word' do
   @word = params[:word]
-  @anagrams = Word.find_anagrams(@word)
+  @anagrams =  Word.find_anagrams(@word)
   erb :show
 end
 
